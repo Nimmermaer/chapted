4b5d82a8d06e3aafb4447c34f5a3455f161efc78
Michael B <michidrau@gmail.com>
2022-12-28 19:26:43 +0100

    [TASK] Clean up ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,190 +1,57 @@
 #
 # Table structure for table 'fe_users'
 #
-CREATE TABLE fe_users (
+CREATE TABLE fe_users
+(
 
-	move int(11) unsigned DEFAULT '0' NOT NULL,
-	challenge int(11) unsigned DEFAULT '0' NOT NULL,
-
-	wins varchar(255) DEFAULT '' NOT NULL,
-	lose varchar(255) DEFAULT '' NOT NULL,
-	custom_color varchar(255) DEFAULT '' NOT NULL,
-	custom_profil int(11) DEFAULT '0' NOT NULL,
-	challenges int(11) unsigned DEFAULT '0' NOT NULL,
-
-	tx_extbase_type varchar(255) DEFAULT '' NOT NULL,
-
+    move            int(11) unsigned DEFAULT '0' NOT NULL,
+    challenge       int(11) unsigned DEFAULT '0' NOT NULL,
+    wins            varchar(255) DEFAULT '' NOT NULL,
+    lose            varchar(255) DEFAULT '' NOT NULL,
+    custom_color    varchar(255) DEFAULT '' NOT NULL,
+    custom_profil   int(11) DEFAULT '0' NOT NULL,
+    challenges      int(11) unsigned DEFAULT '0' NOT NULL,
+    tx_extbase_type varchar(255) DEFAULT '' NOT NULL,
 );
 
 #
 # Table structure for table 'tx_chapted_domain_model_move'
 #
-CREATE TABLE tx_chapted_domain_model_move (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	challenge int(11) unsigned DEFAULT '0' NOT NULL,
-
-	media int(11) unsigned NOT NULL default '0',
-	description text NOT NULL,
-	point int(11) DEFAULT '0' NOT NULL,
-	field int(11) DEFAULT '0' NOT NULL,
-	like_move int(11) DEFAULT '0' NOT NULL,
-	player int(11) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
- KEY language (l10n_parent,sys_language_uid)
-
+CREATE TABLE tx_chapted_domain_model_move
+(
+    challenge   int(11) unsigned DEFAULT '0' NOT NULL,
+    media       int(11) unsigned NOT NULL default '0',
+    description text NOT NULL,
+    point       int(11) DEFAULT '0' NOT NULL,
+    field       int(11) DEFAULT '0' NOT NULL,
+    like_move   int(11) DEFAULT '0' NOT NULL,
+    player      int(11) unsigned DEFAULT '0' NOT NULL,
 );
 
 #
 # Table structure for table 'tx_chapted_domain_model_challenge'
 #
-CREATE TABLE tx_chapted_domain_model_challenge (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	player int(11) unsigned DEFAULT '0' NOT NULL,
-
-	title varchar(255) DEFAULT '' NOT NULL,
-	description text NOT NULL,
-	reckoning varchar(255) DEFAULT '' NOT NULL,
-	likes int(11) DEFAULT '0' NOT NULL,
-	winning_point varchar(255) DEFAULT '' NOT NULL,
-	qr_code varchar(255) DEFAULT '' NOT NULL,
-	latitude varchar(255) DEFAULT '' NOT NULL,
-	longitude varchar(255) DEFAULT '' NOT NULL,
-	moves int(11) unsigned DEFAULT '0' NOT NULL,
-	owner int(11) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
- KEY language (l10n_parent,sys_language_uid)
-
+CREATE TABLE tx_chapted_domain_model_challenge
+(
+    player        int(11) unsigned DEFAULT '0' NOT NULL,
+    title         varchar(255) DEFAULT '' NOT NULL,
+    description   text                    NOT NULL,
+    reckoning     varchar(255) DEFAULT '' NOT NULL,
+    likes         int(11) DEFAULT '0' NOT NULL,
+    winning_point varchar(255) DEFAULT '' NOT NULL,
+    qr_code       varchar(255) DEFAULT '' NOT NULL,
+    latitude      varchar(255) DEFAULT '' NOT NULL,
+    longitude     varchar(255) DEFAULT '' NOT NULL,
+    moves         int(11) unsigned DEFAULT '0' NOT NULL,
+    owner         int(11) unsigned DEFAULT '0' NOT NULL,
 );
 
 #
 # Table structure for table 'tx_chapted_domain_model_table'
 #
-CREATE TABLE tx_chapted_domain_model_table (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	month datetime DEFAULT '0000-00-00 00:00:00',
-	year datetime DEFAULT '0000-00-00 00:00:00',
-	challenges int(11) unsigned DEFAULT '0',
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
- KEY language (l10n_parent,sys_language_uid)
-
-);
-
-#
-# Table structure for table 'tx_chapted_domain_model_challenge'
-#
-CREATE TABLE tx_chapted_domain_model_challenge (
-
-	player  int(11) unsigned DEFAULT '0' NOT NULL,
-
-);
-
-#
-# Table structure for table 'fe_users'
-#
-CREATE TABLE fe_users (
-
-	move  int(11) unsigned DEFAULT '0' NOT NULL,
-
-);
-
-#
-# Table structure for table 'tx_chapted_domain_model_move'
-#
-CREATE TABLE tx_chapted_domain_model_move (
-
-	challenge  int(11) unsigned DEFAULT '0' NOT NULL,
-
-);
-
-#
-# Table structure for table 'fe_users'
-#
-CREATE TABLE fe_users (
-
-	challenge  int(11) unsigned DEFAULT '0' NOT NULL,
-
+CREATE TABLE tx_chapted_domain_model_table
+(
+    month      datetime DEFAULT '0000-00-00 00:00:00',
+    year       datetime DEFAULT '0000-00-00 00:00:00',
+    challenges int(11) unsigned DEFAULT '0',
 );
